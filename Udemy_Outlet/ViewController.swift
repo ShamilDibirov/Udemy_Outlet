381ec1bf7460d3426e8db62c8ca102747720fbd6
Shamil <shamil@MacBook-Pro-Shamil.local>
2017-11-02 19:00:57 +0300

    Initial Commit

---
+++ Udemy_Outlet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udemy_Outlet
+//
+//  Created by Shamil on 02.11.17.
+//  Copyright © 2017 Shamil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
